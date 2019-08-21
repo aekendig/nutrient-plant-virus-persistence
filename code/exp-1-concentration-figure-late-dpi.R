2e270d0a944c4052991be13b3dbfe66147966688
aekendig <aekendig@gmail.com>
2019-08-20 20:34:06 -0400

    combine concentration figures with full and subset datasets

--- code/exp-1-concentration-figure-late-dpi.R
+++ code/exp-1-concentration-figure-late-dpi.R
@@ -10,34 +10,31 @@ library(tidyverse)
 library(ggridges)
 library(cowplot)
 
-# import data
-pdat <- read_csv("./output/exp-1-concentration-analysis-pav-data.csv")
-rdat <- read_csv("./output/exp-1-concentration-analysis-rpv-data.csv")
-
 # load models
+load("./output/exp-1-concentration-analysis-log-informative-rpv.rda")
+load("./output/exp-1-concentration-analysis-log-informative-pav.rda")
 load("./output/exp-1-concentration-analysis-log-informative-late-rpv.rda")
 load("./output/exp-1-concentration-analysis-log-informative-late-pav.rda")
+
+# color palette
+col_pal = c("black", "darkgoldenrod2", "dodgerblue1", "palegreen4")
+
+# text sizes
+sm_txt = 6
+lg_txt = 8
+an_txt = 2
 
 
 #### edit data ####
 
-# inoculation column
-pdat <- pdat %>%
-  mutate(inoculation = ifelse(co == 0, "single", "co"),
-         inoculation = fct_relevel(inoculation, "single"),
-         nutrient = fct_relevel(nutrient, "low", "N", "P"))
-
-rdat <- rdat %>%
-  mutate(inoculation = ifelse(co == 0, "single", "co"),
-         inoculation = fct_relevel(inoculation, "single"),
-         nutrient = fct_relevel(nutrient, "low", "N", "P"))
-
 # posterior samples
-postr <- posterior_samples(m.lid.r)
-postp <- posterior_samples(m.lid.p)
+postr <- posterior_samples(m.li.r)
+postp <- posterior_samples(m.li.p)
+postrd <- posterior_samples(m.lid.r)
+postpd <- posterior_samples(m.lid.p)
 
 # rename columns
-colnames(postr) <- colnames(postp) <- c("int", "co", "N", "P", "co_N", "co_P", "NP", "co_NP", "ar", "sigma", "lp")
+colnames(postr) <- colnames(postp) <- colnames(postrd) <- colnames(postpd) <- c("int", "co", "N", "P", "co_N", "co_P", "NP", "co_NP", "ar", "sigma", "lp")
 
 # category average
 
@@ -73,114 +70,52 @@ avgr <- postr %>%
          Nutrient = factor(Nutrient, levels = c("low", "N", "P", "N+P"))) %>%
   as_tibble()
 
-# percentage increase
-
-slopep <- postp %>%
-  transmute(high_N = (exp(N) - 1),
-            high_P = (exp(P) - 1),
-            high_NP = (exp(N + P + NP) - 1),
-            low_co =  (exp(co) - 1),
-            N_co = (exp(co + co_N) - 1),
-            P_co = (exp(co + co_P) - 1),
-            NP_co = (exp(co + co_N + co_P + co_NP) - 1)) %>%
+avgpd <- postpd %>%
+  transmute(low = int,
+            high_N = int + N,
+            high_P = int + P,
+            high_NP = int + N + P + NP,
+            low_co =  int + co,
+            N_co = int + N + co + co_N,
+            P_co = int + P + co + co_P,
+            NP_co = int + N + P + co + co_N + co_P + NP + co_NP) %>%
   gather(key = "treatment", value = "effect") %>%
   mutate(Inoculation = ifelse(grepl("co", treatment, fixed = T), "coinfection", "single"),
          Inoculation = factor(Inoculation, levels = c("single", "coinfection")),
          Nutrient = recode(treatment, high_N = "N", high_P = "P", high_NP = "N+P", low_co = "low", N_co = "N", P_co = "P", NP_co = "N+P"),
-         Nutrient = factor(Nutrient, levels = c("low", "N", "P", "N+P")))
+         Nutrient = factor(Nutrient, levels = c("low", "N", "P", "N+P"))) %>%
+  as_tibble()
 
-sloper <- postr %>%
-  transmute(high_N = (exp(N) - 1),
-            high_P = (exp(P) - 1),
-            high_NP = (exp(N + P + NP) - 1),
-            low_co =  (exp(co) - 1),
-            N_co = (exp(co + co_N) - 1),
-            P_co = (exp(co + co_P) - 1),
-            NP_co = (exp(co + co_N + co_P + co_NP) - 1)) %>%
+avgrd <- postrd %>%
+  transmute(low = int,
+            high_N = int + N,
+            high_P = int + P,
+            high_NP = int + N + P + NP,
+            low_co =  int + co,
+            N_co = int + N + co + co_N,
+            P_co = int + P + co + co_P,
+            NP_co = int + N + P + co + co_N + co_P + NP + co_NP) %>%
   gather(key = "treatment", value = "effect") %>%
   mutate(Inoculation = ifelse(grepl("co", treatment, fixed = T), "coinfection", "single"),
          Inoculation = factor(Inoculation, levels = c("single", "coinfection")),
          Nutrient = recode(treatment, high_N = "N", high_P = "P", high_NP = "N+P", low_co = "low", N_co = "N", P_co = "P", NP_co = "N+P"),
-         Nutrient = factor(Nutrient, levels = c("low", "N", "P", "N+P")))
-
-# check treatments
-sloper %>% select(treatment, Inoculation, Nutrient) %>% unique()
-
-
-#### figure of raw data ####
-
-# color palette
-col_pal = c("black", "darkgoldenrod2", "dodgerblue1", "palegreen4")
-
-# text sizes
-sm_txt = 6
-lg_txt = 8
-an_txt = 2
-
-# PAV (concentration over time)
-plotA <- ggplot(pdat, aes(x = dpi, y = log_conc, colour = nutrient)) +
-  stat_summary(fun.data = "mean_se", geom = "errorbar", width = 0.1, position = position_dodge(0.6), aes(size = inoculation)) +
-  stat_summary(fun.y = "mean", geom = "point", size = 1.5, position = position_dodge(0.6), aes(shape = inoculation), fill = "white") +
-  stat_summary(fun.y = "mean", geom = "line", position = position_dodge(0.6), aes(linetype = inoculation)) +
-  theme_bw() +
-  theme(axis.title = element_text(color = "black", size = lg_txt),
-        axis.text = element_text(color = "black", size = sm_txt),
-        strip.text = element_blank(),
-        legend.title = element_text(color = "black", size = sm_txt),
-        legend.text = element_text(color = "black", size = sm_txt),
-        legend.position = c(0.42, 0.92),
-        legend.background = element_blank(),
-        legend.key = element_blank(),
-        legend.direction = "horizontal",
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_blank()) +
-  scale_size_manual(values = c(0.5, 0.5), guide = F) +
-  scale_colour_manual(values = col_pal,
-                      name = "Nutrient") +
-  scale_shape_manual(values = c(19, 21), guide = F) +
-  scale_linetype_manual(values = c("solid", "dashed"), guide = F) +
-  xlab("Days post inoculation") +
-  ylab("ln(PAV concentration)")
-
-# RPV (concentration over time)
-plotB <- ggplot(rdat, aes(x = dpi, y = log_conc, colour = nutrient)) +
-  stat_summary(fun.data = "mean_se", geom = "errorbar", width = 0.1, position = position_dodge(0.6), aes(size = inoculation), show.legend = F) +
-  stat_summary(fun.y = "mean", geom = "point", size = 1.5, position = position_dodge(0.6), aes(shape = inoculation), fill = "white") +
-  stat_summary(fun.y = "mean", geom = "line", position = position_dodge(0.6), aes(linetype = inoculation)) +
-  theme_bw() +
-  theme(axis.title = element_text(color = "black", size = lg_txt),
-        axis.text = element_text(color = "black", size = sm_txt),
-        strip.text = element_blank(),
-        legend.title = element_text(color = "black", size = sm_txt),
-        legend.text = element_text(color = "black", size = sm_txt),
-        legend.position = c(0.34, 0.93),
-        legend.background = element_blank(),
-        legend.key = element_blank(),
-        legend.direction = "horizontal",
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        strip.background = element_blank(),
-        legend.key.width = unit(1, "cm")) +
-  scale_size_manual(values = c(0.5, 0.5), name = "Inoculation") +
-  scale_colour_manual(values = col_pal, guide = F) +
-  scale_shape_manual(values = c(19, 21), name = "Inoculation") +
-  scale_linetype_manual(values = c("solid", "dashed"), name = "Inoculation") +
-  xlab("Days post inoculation") +
-  ylab("ln(RPV concentration)")
+         Nutrient = factor(Nutrient, levels = c("low", "N", "P", "N+P"))) %>%
+  as_tibble()
 
 
 #### figure of category averages ####
 
-plotC <- avgp %>%
+plotA <- avgp %>%
   group_by(treatment, Nutrient, Inoculation) %>%
   median_hdi() %>%
   ggplot(aes(x = Nutrient, y = effect,  color = Nutrient)) +
   geom_pointinterval(aes(shape = Inoculation), fatten_point = 2.5, size_range = c(0.4, 0.6), position = position_dodge(0.3), fill = "white", show.legend = F) +
   theme_bw() +
-  theme(axis.title = element_text(color = "black", size = lg_txt),
+  theme(axis.title.y = element_text(color = "black", size = lg_txt),
+        axis.title.x = element_blank(),
         axis.text = element_text(color = "black", size = sm_txt),
         strip.text = element_blank(),
+        plot.title = element_text(color = "black", size = lg_txt, hjust = 0.5),
         legend.title = element_text(color = "black", size = sm_txt),
         legend.text = element_text(color = "black", size = sm_txt),
         legend.background = element_blank(),
@@ -192,9 +127,10 @@ plotC <- avgp %>%
   scale_colour_manual(values = col_pal) +
   scale_shape_manual(values = c(19, 21)) +
   xlab("Nutrient") +
-  ylab("Est. ln(PAV concentration)")
+  ylab("Est. ln(PAV concentration)") +
+  ggtitle("Full dataset")
 
-plotD <- avgr %>%
+plotB <- avgr %>%
   group_by(treatment, Nutrient, Inoculation) %>%
   median_hdi() %>%
   ggplot(aes(x = Nutrient, y = effect,  color = Nutrient)) +
@@ -216,6 +152,56 @@ plotD <- avgr %>%
   xlab("Nutrient") +
   ylab("Est. ln(RPV concentration)")
 
+plotC <- avgpd %>%
+  group_by(treatment, Nutrient, Inoculation) %>%
+  median_hdi() %>%
+  ggplot(aes(x = Nutrient, y = effect,  color = Nutrient)) +
+  geom_point(aes(shape = Inoculation), position = position_dodge(0.3)) +
+  geom_pointinterval(aes(shape = Inoculation), fatten_point = 2.5, size_range = c(0.4, 0.6), position = position_dodge(0.3), fill = "white", show.legend = F) +
+  theme_bw() +
+  theme(axis.title.y = element_text(color = "black", size = lg_txt),
+        axis.title.x = element_blank(),
+        axis.text = element_text(color = "black", size = sm_txt),
+        strip.text = element_blank(),
+        plot.title = element_text(color = "black", size = lg_txt, hjust = 0.5),
+        legend.title = element_text(color = "black", size = sm_txt),
+        legend.text = element_text(color = "black", size = sm_txt),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank(),
+        legend.key.width = unit(1, "cm")) +
+  scale_colour_manual(values = col_pal) +
+  scale_shape_manual(values = c(19, 21), guide = F) +
+  xlab("Nutrient") +
+  ylab("Est. ln(PAV concentration)") +
+  ggtitle("Later dpi dataset")
+
+plotD <- avgrd %>%
+  mutate(Inoculation = recode(Inoculation, coinfection = "co.", single = "sing.")) %>%
+  group_by(treatment, Nutrient, Inoculation) %>%
+  median_hdi() %>%
+  ggplot(aes(x = Nutrient, y = effect,  color = Nutrient)) +
+  geom_point(aes(shape = Inoculation), position = position_dodge(0.3)) +
+  geom_pointinterval(aes(shape = Inoculation), fatten_point = 2.5, size_range = c(0.4, 0.6), position = position_dodge(0.3), fill = "white", show.legend = F) +
+  theme_bw() +
+  theme(axis.title = element_text(color = "black", size = lg_txt),
+        axis.text = element_text(color = "black", size = sm_txt),
+        strip.text = element_blank(),
+        legend.title = element_text(color = "black", size = sm_txt),
+        legend.text = element_text(color = "black", size = sm_txt),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        strip.background = element_blank(),
+        legend.key.width = unit(1, "cm")) +
+  scale_colour_manual(values = col_pal, guide = F) +
+  scale_shape_manual(values = c(19, 21), name = "Inoc.") +
+  xlab("Nutrient") +
+  ylab("Est. ln(RPV concentration)")
+
 
 #### combine plots ####
 
@@ -223,27 +209,12 @@ plotD <- avgr %>%
 plot <- plot_grid(plotA, plotC, plotB, plotD, 
                   labels = c("A", "B", "C", "D"), 
                   label_size = lg_txt, 
-                  rel_widths = c(1, 0.55, 1, 0.55),
+                  rel_widths = c(0.7, 1, 0.7, 1),
                   label_x = c(0, 0, 0, 0))
 
 # print
-pdf("./output/exp-1-concentration-figure-late-dpi.pdf", width = 5, height = 4)
+pdf("./output/exp-1-concentration-figure-late-dpi.pdf", width = 4.75, height = 4)
 plot
 dev.off()
 
-
-#### numbers for text ####
-
-# model summaries
-summary(m.lid.r)
-summary(m.lid.p)
-
-# mean values in proportion change
-sloper %>%
-  group_by(treatment, Inoculation, Nutrient) %>%
-  mean_hdi()
-
-slopep %>%
-  group_by(treatment, Inoculation, Nutrient) %>%
-  mean_hdi()
 
